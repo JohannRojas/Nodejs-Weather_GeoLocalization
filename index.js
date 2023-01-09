754f19224db60a897721ee6072e7cffcbbeffbb4
jhann <johann.rojasu@gmail.com>
2023-01-09 00:02:34 -0500

    patch module

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("HI Jhann")
+import { leerInput } from './helpers/inquirer.js'
+
+const main = async () => {
+  const text = await leerInput('HI Jhann')
+  console.log('🚀 ~ file: index.js:6 ~ main ~ text', text)
+}
+
+main()
